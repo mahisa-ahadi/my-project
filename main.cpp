6e108da51cbfbc3e1aa0a8e122e47351dec053cc
mahisa-ahadi <mahisa.academic@gmail.com>
2025-12-30 20:31:59 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
+//function for fcfs
+
+//function for sjf
+//function for priority
 int main(void){
-cout<<"hello world!";
+
+    ifstream file("input.txt");
+    if(!file){
+        cout<<"file is not found"<<endl;
+        return 1;
+    }
+     int x;
+    while (file >> x) {   // reads integers until EOF
+        cout << x << endl;
+    }
+
+    file.close();
+    
 return 0;
 }
