dc1fed71bd68a3520775aef6a9ea281b6c347d9f
mahisa-ahadi <mahisa.academic@gmail.com>
2025-12-30 21:07:27 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -7,17 +7,7 @@ using namespace std;
 //function for priority
 int main(void){
 
-    ifstream file("input.txt");
-    if(!file){
-        cout<<"file is not found"<<endl;
-        return 1;
-    }
-     int x;
-    while (file >> x) {   // reads integers until EOF
-        cout << x << endl;
-    }
-
-    file.close();
+    cout<<"hello world";
     
 return 0;
 }
